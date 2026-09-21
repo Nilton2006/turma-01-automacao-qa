8992d82bf0697d0859d275f8f850137c11d09746
Nilton2006 <nilton.silva.codai@ufrpe.br>
2026-09-20 23:02:30 -0300

    Adiciona atividade de cenários automatizáveis

---
+++ src/Atividades/automatizaveis.ts
@@ -0,0 +1,33 @@
+import PromptSync from "prompt-sync";
+
+const prompt = PromptSync();
+
+let automatizaveis: number = 0;
+let naoautomatizaveis: number = 0;
+
+for (let i = 1; i <= 5; i++) {
+
+    const cenario: string = prompt(`Digite o ${i}º cenário: `);
+
+    const resposta: string = prompt(`É automatizável? (sim/não) `).toLowerCase();
+
+    if (resposta === "sim") {
+
+        console.log(`${cenario} -> Automatizar`);
+        automatizaveis++;
+
+    } else if (resposta === "não" || resposta === "nao") {
+
+        console.log(`${cenario} -> Não-Automatizar`);
+        naoautomatizaveis++;
+
+    } else {
+
+        console.log("Ocorreu um erro");
+
+    }
+}
+
+console.log(`Total de cenários automatizáveis: ${automatizaveis}`);
+console.log(`Total de cenários não-automatizáveis: ${naoautomatizaveis}`);
+
